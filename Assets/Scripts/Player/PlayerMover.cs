811243d55c6e491745c1d04169a6fa4e47bfc149
RyoUchida <soccer.ry05@gmail.com>
2017-04-03 18:34:55 +0900

    [WIP]プレイヤーの移動処理追加

---
+++ Assets/Scripts/Player/PlayerMover.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMover : MonoBehaviour
+{
+	[SerializeField]
+	private Vector2 _moveSpeed = Vector2.one;
+	public Vector2 MoveSpeed {
+		get { return _moveSpeed; }
+		set { _moveSpeed = value; }
+	}
+
+	void Awake()
+	{
+
+	}
+
+	void Start()
+	{
+
+	}
+
+	void Update()
+	{
+		float horizotal = Input.GetAxis("Horizotal");
+		float vertical = Input.GetAxis("Vecrtical");
+		float deltaTime = Time.deltaTime;
+
+		Vector3 movingAmount = new Vector3(
+			_moveSpeed.x * horizotal,
+			_moveSpeed.y * vertical,
+			0.0f) * deltaTime;
+
+		transform.position += movingAmount;
+
+		// ステージないから出れないようにする処理
+		// 未実装
+	}
+}
