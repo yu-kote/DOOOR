8dbfc731632a0237d018cd2168dd7798f86fbb4d
yu-kote <yu1229kotetu@gmail.com>
2017-04-05 05:11:39 +0900

    道の配列が決まりました。

---
+++ Assets/Scripts/Map/Field.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Field : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _node;
+
+    // 面の、高さの、横
+    private List<List<GameObject>> _nodes = new List<List<GameObject>>();
+
+    public List<List<GameObject>> Nodes { get { return _nodes; } set { _nodes = value; } }
+
+    private int _topFloor = 3;
+
+    private int _loadNum = 40;
+
+
+    private int _interval = 3;
+    private int _heightInterval = 6;
+
+    void Start()
+    {
+        NodesInitialize();
+    }
+
+    private void NodesInitialize()
+    {
+        var pos = new Vector3();
+
+        for (int y = 0; y < _topFloor; y++)
+        {
+            List<GameObject> floor = new List<GameObject>();
+            var surface_num = 0;
+            for (int x = 0; x < _loadNum; x++)
+            {
+                var node = _node;
+
+                var direction = SurfaceDirection(surface_num);
+                pos += new Vector3(_interval * direction.x, 0, _interval * direction.z);
+                
+                node.transform.position = pos;
+
+                floor.Add(Instantiate(node, transform));
+
+                // 曲がり角
+                if (x % (_loadNum / 4) == (_loadNum / 4) - 1)
+                    surface_num++;
+            }
+            pos += new Vector3(0, _heightInterval, 0);
+            _nodes.Add(floor);
+            floor.Clear();
+        }
+    }
+
+    private Vector3 SurfaceDirection(int surface_num)
+    {
+        if (surface_num == 0)
+            return new Vector3(1, 1, 0);
+        if (surface_num == 1)
+            return new Vector3(0, 1, 1);
+        if (surface_num == 2)
+            return new Vector3(-1, 1, 0);
+        if (surface_num == 3)
+            return new Vector3(0, 1, -1);
+        return Vector3.zero;
+    }
+    
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Map/Node.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+
+    // ダイクストラ法 http://www.deqnotes.net/acmicpc/dijkstra/
+
+    // 確定かどうか
+    private bool _isDone;
+    public bool IsDone { get { return _isDone; } set { _isDone = value; } }
+
+    // このノードへの現時点での最小コスト
+    private int _cost;
+    public int Cost { get { return _cost; } set { _cost = value; } }
+
+
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
