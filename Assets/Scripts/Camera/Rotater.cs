fdf74108fb5627b496eb504d4eb3fd49dd50e63a
RyoUchida <soccer.ry05@gmail.com>
2017-04-08 03:24:54 +0900

    CameraRotater -> Rotaterに変更

---
+++ Assets/Scripts/Camera/Rotater.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotater : MonoBehaviour
+{
+	// 一度に回転する角度
+	private const float _rotateAngle = 360.0f / 4.0f;
+	private Vector3 _interestPoint = Vector3.zero;
+
+	[SerializeField]
+	private string _leftRotateButton = "L1";
+	[SerializeField]
+	private string _rightRotateButton = "R1";
+	[SerializeField]
+	private float _rotateTakeTime = 1.0f;
+	private float _time = 0.0f;
+	private float _angle = 0.0f;
+	private bool _isRotating = false;
+	public bool IsRotating
+	{
+		get { return _isRotating; }
+		set { _isRotating = value; }
+	}
+
+	void Start()
+	{
+		// マップの中心点を獲得
+		// 未実装
+
+	}
+
+	void Update()
+	{
+		Rotating();
+
+		if (Input.GetButtonDown(_leftRotateButton))
+			StartRotation(_rotateAngle);
+		if (Input.GetButtonDown(_rightRotateButton))
+			StartRotation(-_rotateAngle);
+	}
+
+	void StartRotation(float rotateAngle)
+	{
+		_isRotating = true;
+		_angle = rotateAngle;
+		_time = 0.0f;
+	}
+
+	void Rotating()
+	{
+		if (!_isRotating)
+			return;
+
+		transform.RotateAround(_interestPoint, Vector3.up, _angle * (Time.deltaTime / _rotateTakeTime));
+		_time += Time.deltaTime / _rotateTakeTime;
+		if (_time < 1.0f)
+			return;
+
+		if(_time > 1.0f)
+		{
+			float overTime = _time - 1.0f;
+			transform.RotateAround(_interestPoint, Vector3.up, -_angle * overTime);
+			transform.eulerAngles = new Vector3(transform.eulerAngles.x,
+				(int)transform.eulerAngles.y,
+				transform.eulerAngles.z);
+		}
+
+		_isRotating = false;
+	}
+}
