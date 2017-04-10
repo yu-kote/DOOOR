1fdbd030ab0fc9f41ca751a14c53804e74de09b9
RyoUchida <soccer.ry05@gmail.com>
2017-04-11 05:51:35 +0900

    Trap生成と発動の簡易的なものの追加

---
+++ Assets/Scripts/Trap/PitFall/PitFall.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PitFall : MonoBehaviour
+{
+	private FootPrint _footPrint = null;
+	public FootPrint FootPrint
+	{
+		get { return _footPrint; }
+		set { _footPrint = value; }
+	}
+
+	private bool _isUsed = false;
+	public bool IsUsed
+	{
+		get { return _isUsed; }
+		set { _isUsed = value; }
+	}
+
+	void Start()
+	{
+		if (_footPrint = null)
+			Debug.Log("_footPrint is null");
+	}
+
+	void Update()
+	{
+		//発動済みだったらはじく
+		if (_isUsed)
+			return;
+		//ノードに人が一人もいなければはじく
+		if (_footPrint.HumansOnNode.Count == 0)
+			return;
+
+		//ここに落とし穴のアニメーション開始処理を記述する
+		//未実装
+
+		_isUsed = true;
+	}
+}
---
+++ Assets/Scripts/Trap/TrapStatus.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum TrapType
+{
+	PITFALLS,
+	NONE
+}
+
+public class TrapStatus : MonoBehaviour
+{
+	private TrapType _type = TrapType.NONE;
+	public TrapType Type
+	{
+		get { return _type; }
+		set { _type = value; }
+	}
+	private bool _isSpawn = false;
+	public bool IsSpawn
+	{
+		get { return _isSpawn; }
+		set { _isSpawn = value; }
+	}
+}
