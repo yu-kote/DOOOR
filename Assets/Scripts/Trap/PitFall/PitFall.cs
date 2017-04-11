4f899c4839f6e26e4566d4357fbeeadbda77d46c
RyoUchida <soccer.ry05@gmail.com>
2017-04-12 00:30:36 +0900

    下のノードを調べ壁だった場合はじく処理を追加

--- Assets/Scripts/Trap/PitFall/PitFall.cs
+++ Assets/Scripts/Trap/PitFall/PitFall.cs
@@ -11,6 +11,12 @@ public class PitFall : MonoBehaviour
 		set { _footPrint = value; }
 	}
 	private NodeManager _nodeManager = null;
+	private Vector2 _nodeCell = Vector2.zero;
+	public Vector2 NodeCell
+	{
+		get { return _nodeCell; }
+		set { _nodeCell = value; }
+	}
 
 	private bool _isUsed = false;
 	public bool IsUsed
@@ -37,11 +43,18 @@ public class PitFall : MonoBehaviour
 		//ノードに人が一人もいなければはじく
 		if (_footPrint.HumansOnNode.Count == 0)
 			return;
+		//一階だったらはじく
+		if (_nodeCell.y == 0)
+			return;
 
 		//ここに落とし穴のアニメーション開始処理を記述する
 		//未実装
 
 		List<List<GameObject>> nodes = _nodeManager.Nodes;
+		GameObject underNode = nodes[(int)_nodeCell.y - 1][(int)_nodeCell.x];
+		//下に壁があれば落ちない
+		if (underNode.GetComponent<Wall>() != null)
+			return;
 
 		Debug.Log("・・・・・・・・・・・・・・・");
 		Debug.Log("落とし穴　作動");
