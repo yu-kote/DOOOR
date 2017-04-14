7b4963de46f341b76f84c5bac882ec35e59dcc0c
RyoUchida <soccer.ry05@gmail.com>
2017-04-15 02:36:15 +0900

    Door作成

---
+++ Assets/Scripts/Map/Attribute/Door.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : AttributeBase
+{
+	public enum DoorStatus
+	{
+		OPEN,
+		CLOSE
+	}
+
+	public DoorStatus _doorStatus = DoorStatus.CLOSE;
+	private Animator anim = null;
+
+	void Awake()
+	{
+		anim = _attribute.GetComponent<Animator>();
+	}
+
+	void Start()
+	{
+		CreateAttribute("Door");
+	}
+
+	void Update()
+	{
+		if (anim.GetCurrentAnimatorStateInfo(0).IsName("OpenIdol") &&
+			anim.GetBool("IsOpen"))
+			anim.SetBool("IsOpen", false);
+
+		if (anim.GetCurrentAnimatorStateInfo(0).IsName("CloseIdol") &&
+			anim.GetBool("IsClose"))
+			anim.SetBool("IsClose", false);
+	}
+
+	public bool StartOpening()
+	{
+		if (_doorStatus != DoorStatus.CLOSE)
+			return false;
+		if (!anim.GetCurrentAnimatorStateInfo(0).IsName("CloseIdol"))
+			return false;
+
+		_doorStatus = DoorStatus.OPEN;
+		anim.SetBool("IsOpen", true);
+
+		return true;
+	}
+
+	public bool StartClosing()
+	{
+		if (_doorStatus != DoorStatus.OPEN)
+			return false;
+		if (!anim.GetCurrentAnimatorStateInfo(0).IsName("OpenIdol"))
+			return false;
+
+		_doorStatus = DoorStatus.CLOSE;
+		anim.SetBool("IsClose", true);
+
+		return true;
+	}
+}
