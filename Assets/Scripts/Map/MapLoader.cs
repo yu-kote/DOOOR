ff6b07229ef35862c00eafcb9f5e239ddbc8f090
RyoUchida <soccer.ry05@gmail.com>
2017-04-16 01:11:43 +0900

    マップをcsv空読み込めるように処理を変更

---
+++ Assets/Scripts/Map/MapLoader.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public enum MapID
+{
+	FLOOR,
+	LEFTSTAIRS,
+	RIGHTSTAIRS,
+	WALL,
+	LEFTDOOR,
+	RIGHTDOOR,
+	START
+}
+
+public class MapLoader : MonoBehaviour
+{
+	[SerializeField]
+	private string _mapFilePath = "PlannerData/Map";
+
+	public List<string[]> _mapDatas = new List<string[]>();
+
+	void Awake()
+	{
+		LoadMap();
+	}
+
+	void LoadMap()
+	{
+		TextAsset csvFile = Resources.Load(_mapFilePath) as TextAsset;
+		StringReader reader = new StringReader(csvFile.text);
+
+		while (reader.Peek() > -1)
+		{
+			string line = reader.ReadLine();
+			_mapDatas.Add(line.Split(','));
+		}
+
+		Debug.Log("sizeY : " + _mapDatas.Count);
+		Debug.Log("sizeX : " + _mapDatas[0].Length);
+	}
+}
