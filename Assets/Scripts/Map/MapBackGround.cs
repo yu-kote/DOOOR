cad8731f63e47be180dac105c8c1b603e5af03a1
yu-kote <yu1229kotetu@gmail.com>
2017-05-31 01:14:18 +0900

    マップの背景が部屋を識別するようになりました。

--- Assets/Scripts/Map/MapBackGround.cs
+++ Assets/Scripts/Map/MapBackGround.cs
@@ -4,25 +4,25 @@ using UnityEngine;
 using System.Linq;
 using System;
 
-public class MapBackGround : MonoBehaviour
+public class MapBackground : MonoBehaviour
 {
     private NodeManager _nodeManager;
 
     [SerializeField]
-    private GameObject _singleBackGround;
+    private GameObject _singleBackground;
     [SerializeField]
-    private GameObject _doubleBackGround;
+    private GameObject _doubleBackground;
 
-    private List<GameObject> _backGrounds = new List<GameObject>();
+    private List<GameObject> _backgrounds = new List<GameObject>();
 
     void Start()
     {
         var field = GameObject.Find("Field");
         _nodeManager = field.GetComponent<NodeManager>();
 
-        _singleBackGround.transform.localScale
+        _singleBackground.transform.localScale
             = new Vector3(_nodeManager.Interval, _nodeManager.HeightInterval, 0);
-        _doubleBackGround.transform.localScale
+        _doubleBackground.transform.localScale
             = new Vector3(_nodeManager.Interval * 2, _nodeManager.HeightInterval, 0);
 
         StartCoroutine(Create());
@@ -37,34 +37,66 @@ public class MapBackGround : MonoBehaviour
             {
                 if (_nodeManager.Nodes[y][x].GetComponent<Corner>())
                     continue;
-                CreateBackGround(_nodeManager.Nodes[y][x]);
+                CreateBackground(_nodeManager.Nodes[y][x]);
             }
         }
     }
 
-    void CreateBackGround(GameObject node)
+    void CreateBackground(GameObject node)
     {
-        var sprites = Resources.LoadAll<Sprite>("Texture/MapTexture");
-        var bg = Instantiate(_singleBackGround, node.transform);
+        GameObject use_bg;
 
+        // 部屋だけ2マスぶんの背景を使う
+        var offset_kyukeispace = 0.0f;
+        if (node.GetComponent<Kyukeispace>())
+        {
+            offset_kyukeispace = -0.1f;
+            BackgroundMaterialChange(1);
+            use_bg = _doubleBackground;
+        }
+        else
+        {
+            BackgroundMaterialChange(0);
+            use_bg = _singleBackground;
+        }
+
+        var bg = Instantiate(use_bg, node.transform);
+
+        // 親子関係ですでに回転と位置は出してあるのでローカルの値を初期化
         bg.transform.localPosition = Vector3.zero;
         bg.transform.localEulerAngles = Vector3.zero;
-
+        
         var offset_pos = new Vector3(0, _nodeManager.HeightInterval / 2, 0);
-
         float offset = _nodeManager.Interval / 2.0f;
 
-        if (_nodeManager.WhichSurfaceNum(node.GetComponent<Node>().CellX) == 0)
-            offset_pos += new Vector3(0, 0, offset);
-        if (_nodeManager.WhichSurfaceNum(node.GetComponent<Node>().CellX) == 1)
-            offset_pos += new Vector3(-offset, 0, 0);
-        if (_nodeManager.WhichSurfaceNum(node.GetComponent<Node>().CellX) == 2)
-            offset_pos += new Vector3(0, 0, -offset);
-        if (_nodeManager.WhichSurfaceNum(node.GetComponent<Node>().CellX) == 3)
-            offset_pos += new Vector3(offset, 0, 0);
-
+        // ジャスタウェイ
+        OffsetSurface(node.GetComponent<Node>().CellX, ref offset_pos, offset);
+        OffsetSurface(node.GetComponent<Node>().CellX, ref offset_pos, offset_kyukeispace);
+        
         bg.transform.position += offset_pos;
 
-        _backGrounds.Add(bg);
+        _backgrounds.Add(bg);
+    }
+
+    void OffsetSurface(int x, ref Vector3 offset_pos, float value)
+    {
+        if (_nodeManager.WhichSurfaceNum(x) == 0)
+            offset_pos += new Vector3(0, 0, value);
+        if (_nodeManager.WhichSurfaceNum(x) == 1)
+            offset_pos += new Vector3(-value, 0, 0);
+        if (_nodeManager.WhichSurfaceNum(x) == 2)
+            offset_pos += new Vector3(0, 0, -value);
+        if (_nodeManager.WhichSurfaceNum(x) == 3)
+            offset_pos += new Vector3(value, 0, 0);
+    }
+
+    /// <summary>
+    /// TODO:背景の識別がintなのでstringに修正予定
+    /// </summary>
+    void BackgroundMaterialChange(int i)
+    {
+        var materials = Resources.LoadAll<Material>("Prefabs/BG/Materials");
+        _singleBackground.GetComponent<MeshRenderer>().material = materials[i];
+        _doubleBackground.GetComponent<MeshRenderer>().material = materials[i];
     }
 }
